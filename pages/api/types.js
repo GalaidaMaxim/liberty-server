a2768cd21d395f7015292419d432ee1346c86366
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2025-07-22 13:06:31 +0300

    patch types and order by in types

--- pages/api/types.js
+++ pages/api/types.js
@@ -25,6 +25,24 @@ const onPOST = async (req, res) => {
   }
 };
 
+const onPATCH = async (req, res) => {
+  try {
+    const userId = await checkJWT(req, res);
+    const { name, id } = req.body;
+    if (!userId || !name || !id) {
+      throw new Error("Missing data");
+    }
+    const result =
+      await sql`UPDATE types SET name = ${name} WHERE id = ${id} RETURNING *`;
+    res.status(200).json(result);
+
+    return;
+  } catch (err) {
+    console.error("Error in PATCH /types:", err.message);
+    res.status(400).json({ error: err.message });
+  }
+};
+
 const onDELETE = async (req, res) => {
   try {
     const id = await checkJWT(req, res);
@@ -54,7 +72,7 @@ const onGET = async (req, res) => {
     }
 
     const result =
-      await sql`SELECT * FROM types WHERE dictionary_id = ${dictionaryId}`;
+      await sql`SELECT * FROM types WHERE dictionary_id = ${dictionaryId} ORDER BY id ASC`;
     res.status(200).json(result);
     return;
   } catch (err) {
@@ -76,6 +94,10 @@ export default async function handler(req, res) {
   }
   if (req.method === "GET") {
     await onGET(req, res);
+    return;
+  }
+  if (req.method === "PATCH") {
+    await onPATCH(req, res);
     return;
   }
   res.status(404).end();
