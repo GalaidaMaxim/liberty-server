d72553c80058f3f2eb6182f0700a431461737258
Maxim Kucheriaviy <maxim-k@i.ua>
2025-07-23 14:25:55 +0300

    add and get words

--- pages/api/words.js
+++ pages/api/words.js
@@ -4,75 +4,96 @@ import sql from "@/db/connection";
 import { checkJWT } from "@/middlevares/JWT";
 
 const onPOST = async (req, res) => {
-	try {
-		const userId = await checkJWT(req, res);
-		const { word, translation, language } = req.body;
+  try {
+    const userId = await checkJWT(req, res);
+    const { word, translation, type_id, dictionaryID } = req.body;
+    console.log(word, translation, type_id, dictionaryID);
 
-		if (!userId || !word || !translation) {
-			throw new Error("Missing data");
-		}
+    if (!userId || !word || !translation) {
+      throw new Error("Missing data");
+    }
 
-		const dictionaryResult = await sql`
-      SELECT id FROM dictionary WHERE user_id = ${userId} AND name = ${language} LIMIT 1
-    `;
+    let wordsResult;
 
-		if (dictionaryResult.length === 0) {
-			throw new Error("Dictionary not found");
-		}
+    if (type_id) {
+      wordsResult = await sql`
+    INSERT INTO words (word, translation, type_id, dictionary_id)
+    VALUES (${word}, ${translation}, ${type_id}, ${dictionaryID})
+    RETURNING *;
+  `;
+    } else {
+      wordsResult = await sql`
+    INSERT INTO words (word, translation, dictionary_id)
+    VALUES (${word}, ${translation}, ${dictionaryID})
+    RETURNING *;
+  `;
+    }
 
-		const dictionaryId = dictionaryResult[0].id;
-
-		const typesResult = await sql` SELECT id FROM types WHERE dictionary_id = ${dictionaryId} LIMIT 1`;
-
-		if (typesResult.length === 0) {
-			throw new Error("Types not found");
-		}
-
-		const typesID = typesResult[0].id;
-
-		const wordsResult = await sql`
-      INSERT INTO words (word, translation, type_id)
-      VALUES (${word}, ${translation}, ${typesID})
-      RETURNING *
-    `;
-
-		res.status(200).json(wordsResult[0]);
-		return;
-	} catch (err) {
-		console.error("Error in POST /types:", err.message);
-		res.status(400).json({ error: err.message });
-	}
+    res.status(200).json(wordsResult[0]);
+    return;
+  } catch (err) {
+    console.error("Error in POST /types:", err.message);
+    res.status(400).json({ error: err.message });
+  }
 };
 
 const onDELETE = async (req, res) => {
-	try {
-		const id = await checkJWT(req, res);
-		const { wordsID } = req.body;
+  try {
+    const id = await checkJWT(req, res);
+    const { wordsID } = req.body;
 
-		if (!id || !wordsID) {
-			throw new Error("no data");
-		}
-		const result = await sql`DELETE FROM types WHERE id = ${wordsID}`;
+    if (!id || !wordsID) {
+      throw new Error("no data");
+    }
+    const result = await sql`DELETE FROM types WHERE id = ${wordsID}`;
 
-		res.status(200).end();
-		return;
-	} catch (err) {
-		console.log(err.message);
-		res.status(400).end();
-		return;
-	}
+    res.status(200).end();
+    return;
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).end();
+    return;
+  }
+};
+
+const onGET = async (req, res) => {
+  try {
+    const id = await checkJWT(req, res);
+    const { dictionaryID, typeID } = req.query;
+    if (!id || !dictionaryID) {
+      throw new Error("no data");
+    }
+    let result;
+    if (typeID) {
+      result =
+        await sql`SELECT * FROM words WHERE dictionary_id = ${dictionaryID} AND type_id = ${typeID} ORDER BY word ASC`;
+    } else {
+      result =
+        await sql`SELECT * FROM words WHERE dictionary_id = ${dictionaryID} ORDER BY word ASC`;
+    }
+    res.status(200).json(result);
+    return;
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).end();
+    return;
+  }
 };
 
 export default async function handler(req, res) {
-	cors(req, res);
-	if (req.method === "POST") {
-		await onPOST(req, res);
-		return;
-	}
-	if (req.method === "DELETE") {
-		await onDELETE(req, res);
-		return;
-	}
-	res.status(404).end();
-	return;
+  cors(req, res);
+  if (req.method === "POST") {
+    await onPOST(req, res);
+    return;
+  }
+  if (req.method === "DELETE") {
+    await onDELETE(req, res);
+    return;
+  }
+  if (req.method === "GET") {
+    await onGET(req, res);
+    return;
+  }
+  res.status(404).end();
+  return;
 }
